f6677368b9a792a509d86a25f22a48b6b230e0e4
teferanatenael <ntefera847@gmail.com>
2023-09-07 06:21:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <studio.h>
 int main(void)
 {
-	puts(*\"Programming is like building a multilingual puzzels*);
-	return 0;
+/**
+ * Hello c
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
