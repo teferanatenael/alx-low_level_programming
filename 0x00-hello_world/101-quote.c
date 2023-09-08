813115f67d08b583ce0a05b2c0a60d38bb87e724
teferanatenael <ntefera847@gmail.com>
2023-09-08 12:37:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 *
@@ -10,8 +10,6 @@
 
 int main(void)
 {
-	char quo[] -"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
