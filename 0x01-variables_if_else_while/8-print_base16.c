179bab1f2ecd2a88c7e0b786d037aff7c1853fe0
teferanatenael <ntefera847@gmail.com>
2023-09-08 16:13:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
