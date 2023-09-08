76185c9e0733a1efe2f4fb6c542f880ad0101823
teferanatenael <ntefera847@gmail.com>
2023-09-08 14:47:09 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int n =97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return(0);
+}
