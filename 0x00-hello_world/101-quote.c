b38d65ea53b8024d868248f44c91c225dc32a9b6
teferanatenael <ntefera847@gmail.com>
2023-09-08 11:48:51 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: the program description
+*
+*Return: Always 1 (Success)
+*
+*/
+
+int main(void)
+{
+	char quo[] -"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
