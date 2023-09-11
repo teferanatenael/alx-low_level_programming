1566a4b41a655e358d0b67304de4dad6e80fbfb9
teferanatenael <ntefera847@gmail.com>
2023-09-10 23:07:21 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -22,7 +22,7 @@ int main(void)
 			{
 				putchar(num_1);
 				putchar(num_2);
-				if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
+				if (num_1 != '9' || (num_1 == '9' && num_2 != '9'))
 				{
 					putchar(',');
 					putchar(' ');
